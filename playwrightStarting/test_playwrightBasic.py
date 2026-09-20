d2ec452e35f8da45c59585ee7d7137834b797544
Mohd Shuja Rizvi <mohdshuja2000@gmail.com>
2026-09-20 18:18:25 +0800

    adding location and selectors using lables

--- playwrightStarting/test_playwrightBasic.py
+++ playwrightStarting/test_playwrightBasic.py
@@ -1,3 +1,4 @@
+import time
 from playwright.sync_api import Page
 
 # this playwright is a fixture provided by the pytest-playwright plugin, its avaiable globally
@@ -10,6 +11,18 @@ def test_playwrightBasic(playwright):
 
 
 # shortcut for the playwright : it will directly provide the page object and will open the browser with context automatically 
-def test_playwrightSHortCur(page:Page):
+def test_playwrightSHortCur(page: Page):
     page.goto("https://www.google.com")
     print(page.title())
+
+
+def test_coreLocators(page: Page):
+    page.goto("https://rahulshettyacademy.com/loginpagePractise/")
+    page.get_by_label("Username").fill("rahulshettyacademy") #input should be wrapped inside <label> tag for this locator to work
+    page.get_by_label("Password").fill("Learning@830$3mK2") #input should be wrapped inside <label> tag for this locator to work
+    page.get_by_role("combobox").select_option("teach") #in this case the dropdown should be inside <select> tag
+    # page.get_by_role("link", name="terms & conditions").click() #if text is in <a> tag, then use get_by_role("link", name="text")
+    # page.get_by_role("checkbox", name="terms").click() #if checkbox is present in the dom, then use get_by_role("checkbox", name="text")
+    page.locator("#terms").check() # using ud to slect the check box
+    page.get_by_role("button", name="Sign In").click() # locates <input type="submit" value="Sign In">
+    time.sleep(5)
